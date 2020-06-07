14512570f5ac399b42eb916c64ff5de785421985
parting_soul <partingsoul@gmail.com>
2020-06-07 15:04:37 +0800

    初步完成三个tab静态页面

---
+++ lib/app/home/tab_home.dart
@@ -0,0 +1,169 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class TabHomeWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TabHomeWidgetState();
+}
+
+class _TabHomeWidgetState extends State<TabHomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQueryData.fromWindow(window).padding.top)),
+          buildTopSearchWidget(),
+          buildHomeWidget(context)
+        ],
+      ),
+    );
+  }
+}
+
+//Widget buildMenu() {
+//  return Row(
+//      mainAxisAlignment: MainAxisAlignment.spaceAround,
+//      children: <Widget>[
+//        Image.asset(
+//          "resource/image/home_pic1.png",
+//          fit: BoxFit.cover,
+//          width: 80,
+//          height: 80,
+//        ),
+//        Image.asset(
+//          "resource/image/home_pic1.png",
+//          fit: BoxFit.cover,
+//          width: 80,
+//          height: 80,
+//        ),
+//        Image.asset(
+//          "resource/image/home_pic1.png",
+//          fit: BoxFit.cover,
+//          width: 80,
+//          height: 80,
+//        ),
+//      ]);
+//}
+
+Widget buildTopSearchWidget() {
+  return Padding(
+      padding: EdgeInsets.only(left: 20, top: 6, right: 20, bottom: 6),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(23.5),
+        child: Container(
+          color: Color(0x33C2BFCF),
+          height: 44,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset("resource/image/home_search.png",
+                    width: 24, height: 24),
+                Text("搜索菜谱/食材", style: TextStyle(color: Color(0xFFC2BFCF)))
+              ]),
+        ),
+      ));
+}
+
+Widget buildHomeWidget(BuildContext context) {
+  return Expanded(
+      child: ListView.builder(
+          padding: EdgeInsets.only(top: 16),
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, bottom: 16),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Stack(children: <Widget>[
+                      Image.asset("resource/image/home_pic1.png",
+                          fit: BoxFit.cover),
+                      Padding(
+                          padding:
+                              EdgeInsets.only(left: 20, top: 24, right: 20),
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "美味营养成功率高的一道家…美味营养成功率高的一道家…美味营养成功率高的一道家…",
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 22),
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.only(top: 10),
+                                  child: Row(
+                                    children: <Widget>[
+                                      ClipOval(
+                                          child: Image.asset(
+                                              "resource/image/zx_pic8.png",
+                                              width: 28,
+                                              height: 28,
+                                              fit: BoxFit.cover)),
+                                      Padding(
+                                          padding: EdgeInsets.only(left: 8),
+                                          child: Text(
+                                            "小圆食记",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white,
+                                                fontSize: 12),
+                                          ))
+                                    ],
+                                  ))
+                            ],
+                          )),
+                      Positioned(
+                          right: 20, bottom: 24, child: CollectedWidget())
+                    ])));
+          },
+          itemCount: 30));
+}
+
+class CollectedWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CollectedWidgetState();
+}
+
+class _CollectedWidgetState extends State<CollectedWidget> {
+  bool isCollected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: _onClick,
+        child: SizedBox(
+            width: 44,
+            height: 44,
+            child: ClipOval(
+                child: DecoratedBox(
+                    decoration: BoxDecoration(color: Color(0xB0F5F5F7)),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset(
+                            "resource/image/ic_collect.png",
+                            width: 19,
+                            height: 19,
+                            color: Color(isCollected ? 0xFF00AD53 : 0xFF84838B),
+                          ),
+                          Text(
+                            "123",
+                            style: TextStyle(
+                                color: Color(
+                                    isCollected ? 0xFF00AD53 : 0xFF84838B),
+                                fontSize: 8),
+                          )
+                        ])))));
+  }
+
+  _onClick() {
+    isCollected = !isCollected;
+    print("2121 $isCollected");
+    setState(() {});
+  }
+}
---
+++ lib/app/news/tab_news.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+class TabNewWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TabNewWidgetState();
+}
+
+class _TabNewWidgetState extends State<TabNewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("资讯")),
+        body: ListView.separated(
+            padding: EdgeInsetsDirectional.only(start: 20, end: 20),
+            itemBuilder: (BuildContext context, int index) {
+              return index % 2 == 0
+                  ? ThreePicNewsItemWidget()
+                  : OnePicNewsItemWidget();
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider(
+                  thickness: 1,
+                  indent: 20,
+                  endIndent: 20,
+                  color: Color(0xFFF5F5F5));
+            },
+            itemCount: 40));
+  }
+}
+
+class OnePicNewsItemWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _OnePicNewsItemWidgetState();
+}
+
+class _OnePicNewsItemWidgetState extends State<OnePicNewsItemWidget> {
+  _OnePicNewsItemWidgetState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(top: 16, bottom: 16),
+        child: SizedBox(
+            width: 96,
+            height: 96,
+            child: Row(
+              textDirection: TextDirection.rtl,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ClipRRect(
+                    child: FadeInImage.assetNetwork(
+                        width: 96,
+                        height: 96,
+                        fit: BoxFit.cover,
+                        placeholder: "resource/image/item_icon.jpg",
+                        image:
+                            "https://puui.qpic.cn/fans_admin/0/3_482668729_1585145086535/0"),
+                    borderRadius: BorderRadius.circular(16)),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "隔离14天没有用！！无症状感染者再度点亮河南隔离14天没有用隔离14天没有用隔离14天没有用",
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    color: Color(0xFF1E192F), fontSize: 16),
+                              ),
+                              Text(
+                                "大观新闻",
+                                style: TextStyle(color: Color(0xFF84838B)),
+                              )
+                            ])))
+              ],
+            )));
+  }
+}
+
+class ThreePicNewsItemWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ThreePicNewsItemWidgetState();
+  }
+}
+
+class _ThreePicNewsItemWidgetState extends State<ThreePicNewsItemWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      final imgWidth = (constraints.maxWidth - 8 * 2) / 3;
+      return Padding(
+          padding: EdgeInsetsDirectional.only(top: 16, bottom: 16),
+          child: Column(children: <Widget>[
+            Padding(
+                padding: EdgeInsets.only(bottom: 12),
+                child: Text(
+                  "杭州天香楼的东坡肉、楼外楼的西湖醋鱼名扬中外杭州天香楼的东坡肉、。在这些杭帮老字号里。在这些杭帮老字号里",
+                  style: TextStyle(color: Color(0xFF1E192F), fontSize: 16),
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                ClipRRect(
+                    child: FadeInImage.assetNetwork(
+                        width: imgWidth,
+                        height: imgWidth,
+                        fit: BoxFit.cover,
+                        placeholder: "resource/image/item_icon.jpg",
+                        image:
+                            "https://puui.qpic.cn/fans_admin/0/3_482668729_1585145086535/0"),
+                    borderRadius: BorderRadius.circular(16)),
+                ClipRRect(
+                    child: FadeInImage.assetNetwork(
+                        width: imgWidth,
+                        height: imgWidth,
+                        fit: BoxFit.cover,
+                        placeholder: "resource/image/item_icon.jpg",
+                        image:
+                            "https://puui.qpic.cn/fans_admin/0/3_482668729_1585145086535/0"),
+                    borderRadius: BorderRadius.circular(16)),
+                ClipRRect(
+                    child: FadeInImage.assetNetwork(
+                        width: imgWidth,
+                        height: imgWidth,
+                        fit: BoxFit.cover,
+                        placeholder: "resource/image/item_icon.jpg",
+                        image:
+                            "https://puui.qpic.cn/fans_admin/0/3_482668729_1585145086535/0"),
+                    borderRadius: BorderRadius.circular(16))
+              ],
+            )
+          ]));
+    });
+  }
+}
